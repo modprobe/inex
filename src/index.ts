1fbebf8320442e58bd6ed26bf2dd55be54fcf496
Alexander Timmermann <alex@timmermann.me>
2025-02-07 19:19:42 +0100

    feat: support reel URLs

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { fetchVideoAlt, fetchVideo, fetchEmbed } from "./service";
 
 const app = express();
 
-app.get("/p/:shortcode", async (req, res) => {
+app.get(["/p/:shortcode", "/reel/:shortcode"], async (req, res) => {
   const { shortcode } = req.params;
 
   const fail = () => res.status(404).send("can't handle this content :(");
